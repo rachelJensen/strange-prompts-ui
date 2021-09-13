cc501d1e577fa3872d2646a88f9589f422024c80
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-09-13 11:12:06 -0600

    Set up files organization

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+import Navigation from '../Navigation/Navigation';
+
+const Header = () => {
+  return (
+      <div className="header-section">
+          <h2>Header Section</h2>
+          <Navigation />
+      </div>
+  )
+}
+
+export default Header;
