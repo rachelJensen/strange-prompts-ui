aa74b91ed2cd0a73aa90361c941d7e038eaa5c2f
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-09-16 08:53:31 -0600

    Conver functional components into class based components

--- src/components/PromptSection/PromptSection.js
+++ src/components/PromptSection/PromptSection.js
@@ -1,15 +1,27 @@
 import './PromptSection.css';
+import React, { Component } from 'react';
 import PromptForm from '../PromptForm/PromptForm';
 import RandomPrompt from '../RandomPrompt/RandomPrompt';
+import promptsData from '../prompts-data'
 
-const PromptSection = () => {
-  return (
-      <div className="prompt-section">
-          <button>Generate New Prompt</button>
-          <RandomPrompt />
-          <PromptForm />
-      </div>
-  )
+class PromptSection extends Component {
+  constructor(){
+    super()
+    this.state = {
+      promptsData: [],
+      randomPrompt: ''
+    }
+  }
+
+  render() {
+    return (
+       <div className="prompt-section">
+           <button>Generate New Prompt</button>
+           <RandomPrompt />
+           <PromptForm />
+       </div>
+    )
+  }
 }
 
 export default PromptSection;
