4bb60aa04b17bd7f8f18020794242f8bda8553ae
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-09-16 08:45:33 -0600

    Incorporate mock data for test

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,12 +4,14 @@ import PromptSection from '../PromptSection/PromptSection';
 import PromptContainer from '../PromptContainer/PromptContainer';
 import Error from '../Error/Error';
 import { Route, Switch } from 'react-router-dom';
+import promptsData from '../prompts-data'
 
 const App = () => {
+  console.log(promptsData)
   return (
     <div className="App">
       <Header />
-      <main classname="main-section">
+      <main className="main-section">
         <Switch>
           <Route exact path="/" render={ () =>  <PromptSection />}/>
           <Route exact path="/saved-prompts" render={ () => <PromptContainer />}/>
