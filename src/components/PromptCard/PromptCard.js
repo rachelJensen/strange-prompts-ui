f45ae47e3e30b5d1344a5308bf3f5c4b83ec058d
Rachel Jensen <rachelangela@gmail.com>
2021-09-17 18:43:36 -0600

    Build PromptCard

--- src/components/PromptCard/PromptCard.js
+++ src/components/PromptCard/PromptCard.js
@@ -1,10 +1,10 @@
 import './PromptCard.css';
 
-const PromptCard = ({id, key, description}) => {
+const PromptCard = ({id, character, setting, problem}) => {
   return (
-      <div className="prompt-card-section">
-          <h4>PromptCard Section</h4>
-          <p>{description}</p>
+      <div className="prompt-card-section" key={id}>
+          {/* <h4>PromptCard Section</h4> */}
+          <p>{character} {setting} {problem}</p>
       </div>
   )
 }
