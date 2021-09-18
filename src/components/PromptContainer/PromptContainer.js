3b1f2c84ae7be4a9ba25d2ca17618a691fd3f873
Rachel Jensen <rachelangela@gmail.com>
2021-09-17 18:44:04 -0600

    Add favorites from API

--- src/components/PromptContainer/PromptContainer.js
+++ src/components/PromptContainer/PromptContainer.js
@@ -1,5 +1,5 @@
 import './PromptContainer.css';
-import '../PromptCard/PromptCard';
+import PromptCard from '../PromptCard/PromptCard'
 import React, { Component } from 'react';
 import getData from '../utils';
 
@@ -8,31 +8,52 @@ class PromptContainer extends Component {
   constructor() {
     super();
     this.state = {
-      favorites: []
+      allPrompts: [],
+      favorites: [],
+      error: ''
     }
   }
+
   componentDidMount = () => {
-    getData('https://strange-prompts-api.herokuapp.com/api/v1/favorites')
-    .then(data => this.setState({ favorites: data }))
-    //this is not currently a part of the express server
+    // getData('https://strange-prompts-api.herokuapp.com/api/v1/favorites')
+    //   .then(data => this.setState({ favorites: data }))
+    //   .then(data => console.log(this.state.favorites, '<<favorites'))
+      
+    getData('https://strange-prompts-api.herokuapp.com/api/v1/prompts')
+      .then(data => this.setState({ allPrompts: data }))
+      .then(data => getData('https://strange-prompts-api.herokuapp.com/api/v1/favorites')
+        .then(data => this.makeFavorites(data))
+        .then(faves => this.setState({favorites: faves}))
+        )
+  }
+
+  makeFavorites = (favorites) => {
+    return favorites.map((prompt) => {
+      return {
+        id: prompt.id,
+        character: this.state.allPrompts[prompt.character].character,
+        setting: this.state.allPrompts[prompt.setting].setting,
+        problem: this.state.allPrompts[prompt.problem].problem
+      }
+    })
   }
   
   render = () => {
-    
-    // const favorites = this.state.favorites.map(prompt => {
-    //     return (
-    //       <PromptCard
-    //         id={prompt.id}
-    //         key={prompt.id}
-    //         info={prompt.info}
-    //       />
-    //     )
-    //   })
+    const faveCards = this.state.favorites.map(compiled => {
+      return (
+          <PromptCard
+            id={compiled.id}
+            character={compiled.character}
+            setting={compiled.setting}
+            problem={compiled.problem}
+          />
+        )
+    })
     
     return (
      <div className='prompts-collection'>
-         <h4>No saved prompts !</h4>
-       {/* {promptsCollection} */}
+        {/* {this.state.error && <h2>Oh no!</h2>} */}
+        {!this.state.favorites ? <h3>Loading…</h3> : faveCards}
      </div>
    )
   }
