97f3d715b05ca3774ea89f376925bfa9271fc18a
Rachel Jensen <rachelangela@gmail.com>
2021-09-17 18:47:53 -0600

    Refactor error

--- src/components/utils.js
+++ src/components/utils.js
@@ -4,9 +4,29 @@ const getData = (url) => {
       if (response.ok) {
         return response.json();
       } else {
-        return Promise.reject(response.status)
+        return Promise.reject(`error ${response.status}`)
       }
     })
 } 
 
+
+// const getFavorites = (body) => {
+//   return fetch('https://strange-prompts-api.herokuapp.com/api/v1/favorites', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(body)
+//   })
+//   .then(res => res.json())
+//   .then(res => console.log(res))
+//   .catch(error => console.log(error))
+// }
+
 export default getData;
+
+// {
+//   character: 2,
+//   setting: 4,
+//   problem: 7
+// }
