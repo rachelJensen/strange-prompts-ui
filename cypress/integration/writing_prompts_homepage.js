518c645fd9a7913c0ada7137325b379682a9cfc3
Samantha Brown <bro7445@gmail.com>
2021-09-19 10:39:48 -0400

    Add navbar and bookmark tests

--- cypress/integration/writing_prompts_homepage.js
+++ cypress/integration/writing_prompts_homepage.js
@@ -3,4 +3,11 @@ describe('User writing prompt flows', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
   })
+  it('Navbar should contain website title', () => {
+      cy.get('h1').contains('Strange Prompts for a Strange Planet')
+  })
+  it('Should be able to click on favorites bookmark page updates URL and display details', () => {
+     cy.get('bookmark').click()
+     cy.url().should('not.eq', 'http://localhost:3000/')
+   })
 })
