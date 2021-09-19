1fb3e865828925c088e77e84dcf87fbe0466deb5
Samantha Brown <bro7445@gmail.com>
2021-09-19 11:46:01 -0400

    Corrects html element for favorite button

--- cypress/integration/writing_prompts_homepage.js
+++ cypress/integration/writing_prompts_homepage.js
@@ -1,6 +1,5 @@
 describe('User writing prompt flows', () => {
 
-
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
@@ -13,10 +12,10 @@ describe('User writing prompt flows', () => {
       cy.get('h1').contains('Strange Prompts')
   })
 
-  it('Should be able to click on favorites bookmark page updates URL and display favorites', () => {
-     cy.get('Favorites').click()
+  it('Should be able to click on favorites bookmark and page updates URL and display favorites', () => {
+     cy.get('.nav-btns').click()
      .should('have.class', 'nav-btns')
-     cy.url().should('not.eq', 'http://localhost:3000/')
+     cy.url().should('not.eq', 'http://localhost:3000')
   })
 
    it('Should be able to display random writing prompts on load', () => {
