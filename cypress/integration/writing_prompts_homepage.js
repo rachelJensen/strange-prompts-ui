4ad178ff128c8c6b0078f7de3250dced04aae93e
Samantha Brown <bro7445@gmail.com>
2021-09-19 12:42:30 -0400

    Add button test statement but not the test

--- cypress/integration/writing_prompts_homepage.js
+++ cypress/integration/writing_prompts_homepage.js
@@ -12,7 +12,7 @@ describe('User writing prompt flows', () => {
       cy.get('h1').contains('Strange Prompts')
   })
 
-  it('Should be able to click on favorites bookmark and page updates URL and display favorites', () => {
+  it('Should be able to click on favorites bookmark and page updates URL and displays favorites', () => {
      cy.get('.nav-btns').click()
      .should('have.class', 'nav-btns')
      cy.url().should('not.eq', 'http://localhost:3000')
@@ -20,5 +20,13 @@ describe('User writing prompt flows', () => {
 
    it('Should be able to display random writing prompts on load', () => {
      cy.get('.prompt-section').should('be.visible')
+  })
+
+  it('Should add shown prompt to favorites when add to favorites button is clicked', () => {
+    //cy.
+  })
+
+  it('Should show prompt when generate new prompt button is selected', () => {
+    
   })
 })
