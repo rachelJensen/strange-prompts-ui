697961e9ec70db8172f41956819bc522fb8fce24
Samantha Brown <bro7445@gmail.com>
2021-09-19 11:25:52 -0400

    Add should confirm true is equal to true test

--- cypress/integration/writing_prompts_homepage.js
+++ cypress/integration/writing_prompts_homepage.js
@@ -1,13 +1,25 @@
 describe('User writing prompt flows', () => {
 
   beforeEach(() => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('http://localhost:3000')
   })
+
+  it('Should confirm that true is equal to true', () => {
+    expect(true).to.equal(true)
+  })
+
   it('Navbar should contain website title', () => {
       cy.get('h1').contains('Strange Prompts for a Strange Planet')
   })
-  it('Should be able to click on favorites bookmark page updates URL and display details', () => {
-     cy.get('bookmark').click()
+
+  it('Should be able to click on favorites bookmark page updates URL and display favorites', () => {
+     cy.get('Favorites').click()
+     .should('have.class', 'nav-btns')
      cy.url().should('not.eq', 'http://localhost:3000/')
-   })
+  })
+
+   it('Should be able to display random writing prompts on load', () => {
+     cy.get('PromptSection').should('be.visible')
+  })
+
 })
